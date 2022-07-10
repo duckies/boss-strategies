81999a7149dd8464597901bd5f563a6234bf9435
Seikcud <gilbert.johnt@gmail.com>
2022-07-10 19:12:20 -0400

    feat: style inline code

---
+++ src/components/content/ProseCodeInline.vue
@@ -0,0 +1,17 @@
+<template>
+  <code><slot /></code>
+</template>
+
+<style lang="scss" scoped>
+code {
+  background-color: rgb(250, 250, 250);
+  color: rgb(24, 24, 27);
+  padding: 3px 6px;
+  border-radius: 0.375rem;
+}
+
+.dark code {
+  background-color: rgb(24, 24, 27);
+  color: rgb(250, 250, 250);
+}
+</style>
